bc7d66598103c622b68af01b3a232a2982beaf95
Gabriel Rosemberg <gabriel.f.rosemberg@gmail.com>
2025-03-18 22:33:20 -0300

    Desafio implementado dentro do app.js

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+  const amigoInput = document.getElementById("amigo");
+  const amigoNome = amigoInput.value;
+
+  if (amigoNome === "") {
+    alert("Por favor, insira um nome válido.");
+    return;
+  }
+
+  amigos.push(amigoNome);
+  amigoInput.value = "";
+
+  const listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
+
+  amigos.forEach((amigo) => {
+    const li = document.createElement("li");
+    li.textContent = amigo;
+    listaAmigos.appendChild(li);
+  });
+}
+
+function sortearAmigo() {
+
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = "";
+
+  const indiceSorteado = Math.floor(Math.random() * amigos.length);
+  const amigoSorteado = amigos[indiceSorteado];
+
+  const li = document.createElement("li");
+  li.textContent = `O amigo sorteado é: ${amigoSorteado}`;
+  resultado.appendChild(li);
+}
